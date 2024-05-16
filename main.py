846cb6b45f8ba25c9fc06bf3df81a5ac69c01304
Tommi Saltiola <tommi@tommisaltiola.com>
2024-05-16 17:28:54 +0000

    16 May 2024, 11:28

--- main.py
+++ main.py
@@ -6,15 +6,8 @@ import os
 # Load environment variables from .env file
 load_dotenv('cfg/.env')
 
-def query_clickhouse(sql, url, headers, auth, method='POST'):
-    response = requests.request(method, url, data=sql, headers=headers, auth=auth)
-    if response.text:
-        return response.text.splitlines()
-    else:
-        return []
-
-def fetch_data(query, table):
-    clickhouse_config = {
+def get_clickhouse_config():
+    return {
         "url": os.getenv("CLICKHOUSE_URL"),
         "username": os.getenv("CLICKHOUSE_USERNAME"),
         "password": os.getenv("CLICKHOUSE_PASSWORD"),
@@ -22,6 +15,16 @@ def fetch_data(query, table):
             "Content-Type": "application/x-www-form-urlencoded"
         }
     }
+
+def query_clickhouse(sql, url, headers, auth, method='POST'):
+    response = requests.request(method, url, data=sql, headers=headers, auth=auth)
+    if response.text:
+        return response.text.splitlines()
+    else:
+        return []
+
+def fetch_data(query):
+    clickhouse_config = get_clickhouse_config()
     auth = (clickhouse_config['username'], clickhouse_config['password'])
     return query_clickhouse(query, clickhouse_config['url'], clickhouse_config['headers'], auth)
 
@@ -39,14 +42,7 @@ def generate_combinations(programmatic_structure, cities):
     return all_combinations
 
 def insert_into_clickhouse(sentences):
-    clickhouse_config = {
-        "url": os.getenv("CLICKHOUSE_URL"),
-        "username": os.getenv("CLICKHOUSE_USERNAME"),
-        "password": os.getenv("CLICKHOUSE_PASSWORD"),
-        "headers": {
-            "Content-Type": "application/x-www-form-urlencoded"
-        }
-    }
+    clickhouse_config = get_clickhouse_config()
     auth = (clickhouse_config['username'], clickhouse_config['password'])
     # Batch insert
     values_str = ', '.join(f"('{s}')" for s in sentences)
@@ -55,8 +51,8 @@ def insert_into_clickhouse(sentences):
 def main():
     programmatic_structures_query = "SELECT programmatic_structure FROM default.lvi_programmatic_structures"
     cities_query = "SELECT city FROM default.fi_cities"
-    programmatic_structures = fetch_data(programmatic_structures_query, 'lvi_programmatic_structures')
-    cities = fetch_data(cities_query, 'fi_cities')
+    programmatic_structures = fetch_data(programmatic_structures_query)
+    cities = fetch_data(cities_query)
 
     all_sentences = []
     for structure in programmatic_structures:
